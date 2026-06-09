7dcaef7f1d2461d015c3f8675ac906361bfb2cc8
Osisgora <osisgora97@gmail.com>
2026-06-09 15:56:04 -0600

    Reorganización de estructura del proyecto

--- PYTHON/concatenar.py
+++
@@ -1,6 +0,0 @@
-nombre="oscar"
-carrera="Ing.Civil"
-lugar="Mi casa"
-cancion="love"
-sentimiento="feliz"
-print("Hola, soy {} estudio {}. Mi lugar favorito es {}. Y mi canción favorita es {} porque me pone {}".format(nombre,carrera,lugar,cancion,sentimiento))
