f5d5fa4faa8cebe000893a51de09087f11f9819c
OscarIsaacGomezMora <osisgora97@gmail.com>
2026-06-12 23:41:25 -0600

    Clase 19

--- chupamon.js
+++ chupamon.js
@@ -1,5 +1,19 @@
 function seleccionarMascotaJugador(){
-    alert ('Seleccionaste tu mascota')
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let imputCapapipepo = document.getElementById('Capapipepo')
+    let imputRatigueya = document.getElementById('Ratigueya')
+    let imputLangostelvis = document.getElementById('Langostelvis')
+    if (inputHipodoge.checked){
+        alert ('seleccionaste a Hipodoge')
+    } else if (imputCapapipepo.checked){
+        alert ('seleccionaste a Capapipepo')
+    } else if (imputRatigueya.checked){
+        alert ('seleccionaste a Ratigueya')
+    } else if (imputLangostelvis.checked){
+        alert ('seleccionaste a Langostelvis')
+    } else {
+        alert ('No has seleccionado ningún personaje')
+    }
 }
 function iniciarJuego (){
     let botonMascotaJugador = document.getElementById('boton-mascota')
