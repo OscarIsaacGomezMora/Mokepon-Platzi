ce308cce3db620dc46f18771b1bb02e62566e265
Oscar Isaac Gomez Mora <osisgora97@gmail.com>
2026-06-15 15:59:58 -0600

    Clase 20 y 21. Se usó span y innerHtml para seleccionar la mascota el jugador y la computadora

--- chupamon.js
+++ chupamon.js
@@ -3,17 +3,42 @@ function seleccionarMascotaJugador(){
     let imputCapapipepo = document.getElementById('Capapipepo')
     let imputRatigueya = document.getElementById('Ratigueya')
     let imputLangostelvis = document.getElementById('Langostelvis')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
     if (inputHipodoge.checked){
-        alert ('seleccionaste a Hipodoge')
+        spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if (imputCapapipepo.checked){
-        alert ('seleccionaste a Capapipepo')
+        spanMascotaJugador.innerHTML = 'Capapipepo'
     } else if (imputRatigueya.checked){
-        alert ('seleccionaste a Ratigueya')
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     } else if (imputLangostelvis.checked){
-        alert ('seleccionaste a Langostelvis')
+        spanMascotaJugador.innerHTML = 'Langostelvis'
     } else {
-        alert ('No has seleccionado ningún personaje')
+        alert ('Selecciona una mascota')
     }
+
+    seleccionarMascotaEnemigo ()
+}
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio (1,4)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capapipepo'
+
+    } else if (ataqueAleatorio == 3) {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Langostelvis'
+    }
+}
+
+function aleatorio(min,max) {
+    return Math.floor(Math.random()*(max - min + 1))
 }
 function iniciarJuego (){
     let botonMascotaJugador = document.getElementById('boton-mascota')
